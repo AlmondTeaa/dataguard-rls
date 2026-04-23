e7dc92ef67ac8148571af7c86cacad075aefad55
josh-jrati <jos.constantine.almonte@jrati.com>
2026-04-23 16:13:24 +0800

    feat: /health endpoint done

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -3,13 +3,16 @@ use axum::{
     Router,
 };
 
+use routes;
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new()
+        .route("/health", get(routes::health));
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    println!("Server Running");
     axum::serve(listener, app).await.unwrap();
 }
